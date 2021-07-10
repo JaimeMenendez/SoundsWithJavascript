614420155df7af69cfaa4c33dfc305abe38cd17c
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-07-10 17:15:01 -0500

    Added clicked action

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
-let buttons = this.document.getElementsByClassName('Boton');
-console.log(buttons)
+const buttons = document.getElementsByClassName('Boton');
+Array.prototype.forEach.call(buttons, b => {
+    b.addEventListener('click', function (event) {
+    const audio = document.getElementById(`${b.id.slice(0,4)}`);
+    b.classList.add('playing');
+
+    audio.currentTime = 0;
+    audio.play();
+    b.addEventListener('transitionend', e => {
+        b.classList.remove('playing');})
+
+    })});
+
 
 window.addEventListener('keypress', function (e) {
     const audio = document.getElementById(`${e.code}`);
     const button = document.getElementById(`${e.code}B`);
-    
+
     button.classList.add('playing');
-    if(!audio) return;
+    if (!audio) return;
 
     audio.currentTime = 0;
     audio.play();
 
     Array.from(buttons).forEach(boton => {
-        boton.addEventListener('transitionend',e => {
+        boton.addEventListener('transitionend', e => {
             boton.classList.remove('playing');
         })
     });
